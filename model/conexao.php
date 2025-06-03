a75567ced5c1f2fa7a661d454338650cf230a1f2
Letícia Sampaio <leticia.sampaio.fagundes@gmail.com>
2025-06-03 12:44:51 -0300

    Update conexao.php

--- model/conexao.php
+++ model/conexao.php
@@ -1,6 +1,6 @@
 <?php 
 try{
-    $pdo = new PDO('mysql:host=localhost;dbname=Usuario_e_livros', 'gusta', 'gusta');
+    $pdo = new PDO('mysql:host=localhost;dbname=Usuario_e_livros', 'root', '');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     /*$MSQL = $pdo->prepare("INSERT INTO cadastroUsuario.tabelaUsuarios (nomeUsu, cpf, email, numero) VALUES ('Fernando Nunes', 12345678910, 'Gusta2006@gmail.com', 41996966829)");
